8f5440b066cca68090e9ae9e8829523cebab840e
PhAlves23 <ph23.alves@gmail.com>
2021-10-23 23:44:15 -0300

    estudando crud

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_1280.png',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Rafael',
+    email: 'rafael@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_1280.png',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Fernanda',
+    email: 'fernanda@zymail.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2021/09/24/22/05/woman-6653634_1280.jpg',
+  ),
+  '4': const User(
+    id: '4',
+    name: 'Matheus',
+    email: 'matheusobrabo@zymail.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/12/13/16/17/dancer-1904467_1280.png',
+  ),
+  '5': const User(
+    id: '5',
+    name: 'Ana Beatriz',
+    email: 'anabia23@gmail.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-1300582_1280.png',
+  ),
+  '6': const User(
+    id: '6',
+    name: 'Vanessa Souza',
+    email: 'vanessa@zymail.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2021/10/14/08/14/child-6708418_1280.png',
+  ),
+};
